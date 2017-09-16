84fb1670d1de3e66bbc63eed6860ecc6753a0a0c
Martin Balch <s179122@students.aie.edu.au>
2017-09-15 17:15:46 -0700

    first commit

---
+++ Source/section_3/section_3.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "section_3.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, section_3, "section_3" );
